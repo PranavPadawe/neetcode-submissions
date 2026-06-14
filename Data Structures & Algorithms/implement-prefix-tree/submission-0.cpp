b5c9cfdf7e4b533c5c3632404bc5ed0fa67c27f4
Pranav Padawe <75621459+PranavPadawe@users.noreply.github.com>
2026-06-14 19:33:50 +0530

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,65 @@
+struct Node{
+    Node * links[26];
+    bool flag = false;
+    bool containsKey(char ch){
+        return links[ch - 'a']!=NULL;
+    }
+
+    void put(char ch, Node *node){
+        links[ch - 'a'] = node;
+    }
+
+    Node *get(char ch){
+        return links[ch - 'a'];
+    }
+    
+    void setEnd(){
+        flag = 1;
+    }
+
+    bool isEnd(){
+        return flag;
+    }
+
+};
+class PrefixTree {
+private:
+    Node* root;
+public:
+    PrefixTree() {
+        root = new Node();
+    }
+    
+    void insert(string word) {
+        Node *node = root;
+        for(auto it: word){
+            if(!node->containsKey(it)){
+                node->put(it, new Node());  
+            }
+            node = node->get(it);
+        }
+        node->setEnd();
+    }
+    
+    bool search(string word) {
+        Node *node = root;
+        for(auto it: word){
+            if(!node->containsKey(it)){
+                return false;
+            }
+            node = node->get(it);
+        }
+        return node->isEnd();
+    }
+    
+    bool startsWith(string prefix) {
+        Node *node = root;
+        for(auto it: prefix){
+            if(!node->containsKey(it)){
+                return false;
+            }
+            node = node->get(it);
+        }
+        return true;
+    }
+};
